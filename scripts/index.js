1f24a3f85eb3d73c7efda09e1241319c24cf8a40
AChirkova <leadenrain@yandex.ru>
2024-05-11 12:28:04 +0500

    addCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,33 @@
+const cardsList = document.querySelector('.places__list');
+const deleteCard = (event) => {
+    event.target.closest('.card').remove ();
+}
+
+function addCard(cardData, deleteCard) {
+
+    const cardTemplate = document.querySelector('#card-template').content;
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    
+    const cardTitle = card.querySelector('.card__title');
+    const cardImage = card.querySelector('.card__image');
+
+    cardTitle.textContent = cardData.name;
+    cardImage.src = cardData.link
+
+    const deleteButton = card.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCard);
+
+    cardsList.append(card);
+}
+
+initialCards.forEach(function (cardData) {
+    addCard(cardData, deleteCard);
+})
+
+    
+
+
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
@@ -7,3 +37,4 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
